1d69e5d3ac7fa29cc6475c01fd91fe5ed66d7d0f
Prem <sirsikarp@gmail.com>
2023-08-03 10:24:48 +0530

    first commit

---
+++ src/com/gti/demo/Test1.java
@@ -0,0 +1,18 @@
+package com.gti.demo;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		String hungrry="Yes";
+		
+		if(hungrry=="Yes")
+		{
+			System.out.println("eat it");
+		}
+		else
+		{
+			System.out.println("do some work");
+		}
+	}
+
+}
